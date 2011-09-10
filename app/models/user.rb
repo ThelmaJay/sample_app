7a7d6bd92079deb875f2987e9acbc009c2163b05
Sandra Milhais <arcanjo.mor@gmail.com>
2011-09-10 19:42:43 +0100

    User model with passwords, and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,36 @@ class User < ActiveRecord::Base
 	#Return true if the user's password matches the submitted password
 	def has_password?(submitted_password)
 		#Compare encrypted_password with the encrypted version of submitted_password
+		encrypted_password == encrypt(submitted_password)
 	end
 
+	def self.authenticate(email, submitted_password)
+		user = find_by_email(email)
+		return nil if user.nil?
+		return user if user.has_password?(submitted_password)
+	end
+	
 
 	private
 
 		def encrypt_password
+			#new_record? - returns true if the object has not yet been saved to the database.
+			#              Since the salt is a unique identifier for each user, we don't wanna it to change every time the user is updated.
+			#	       With this method we ensure that the salt is only created once, when the user is first created. 
+			self.salt = make_salt if new_record?
 			self.encrypted_password = encrypt(password)
 		end
 
-		def encrypt(string)
-			string #Only a temporary implementation!
+		def encrypt(string) #related to submitted_password
+			secure_hash("#{salt}--#{string}")
+		end
+
+		def make_salt #related to encrypted_password
+			secure_hash("#{Time.now.utc}--#{password}")
+		end
+
+		def secure_hash(string)
+			Digest::SHA2.hexdigest(string)
 		end
 end
 
