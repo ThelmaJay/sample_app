adeaec0fa15f018e7ff358dfb3179e9f3db21083
Sandra Milhais <arcanjo.mor@gmail.com>
2011-11-02 21:06:45 +0000

    CH11.5.1 solved - javascript display to count down from 140 characters added - with comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,6 +20,18 @@ module ApplicationHelper
 	   #var field_id => Conteúdo escrito até agora.
 	   #var update_id => campo que mostra qtos caracteres faltam para atingir o max.
 	   
+	   
+	   #.innerHTML actualiza o contador (char_count_out_id no app/views/shared/_micropost_form.html.erb) com o número de caracteres 
+	   #que ainda faltam para os 140.
+     #innerHTML -> By manipulating an element's innerHtml you'll be able to change the contents of an HTML element.
+	   
+	   # javascript_tag() -> Returns a JavaScript tag with the content inside.
+     # O que se esta aqui a fazer é a correr a função dentro de um Script. 
+     # O número de caracteres que ainda faltam para atingir o maximo ou o número de carateres em excesso é colocado na var out.
+     
+     #A linha observe_field é reponsável pela actualização em real time do número de caracteres em falta ou em excesso.
+
+	   
     function = "var out = []; 
                 var remaining_chars = $(#{maximum_number_of_chars}) - $F('#{field_id}').length; 
                 
@@ -31,7 +43,6 @@ module ApplicationHelper
                   out = 'Too long by ' + remaining_chars; 
                   document.getElementById('#{field_id}').style.backgroundColor='red' 
                 }
-
                 $('#{update_id}').innerHTML = out;"
 
     out = javascript_tag(function)
